b0f76b2ef5958936bf6506726196d3698d44cc08
Ma Chao <cnfhv1988@sina.com>
2017-10-18 10:37:46 +0800

    Update FactoryPattern.js

--- JS/FactoryPattern.js
+++ JS/FactoryPattern.js
@@ -1,3 +1,8 @@
+
+//工厂模式好处，弱化对象间的耦合，减少代码重复，但是切勿滥用
+//一般做法是，创建抽象父类，在子类中实现工厂方法，这样提供了一种灵活性，可以在运行期间自由选择实现
+
+
 var AjaxHandler = new Interface('AjaxHandler',['request','createXhrObject']);
 
 var SimpleHandler = function () {};
@@ -43,6 +48,8 @@ SimpleHandler.prototype = {
     }
 }
 
+
+//处理器类，应对高网络延迟
 var QueueHandler = function () {
     this.queue = [];
     this.requestInProgress = false;
@@ -53,6 +60,102 @@ extend(QueueHandler, SimpleHandler);
 
 QueueHandler.prototype.request = function (method,url,callback,postVars,override) {
     if(this.requestInProgress && !override){
-
+        //if override = true,the request will be send immediately, else override = false / undefined,
+        //the request will be push into the queue
+        this.queue.push({method:method,url:url,callback:callback,postVars:postVars});
+    }
+    else{
+        this.requestInProgress = true;
+        var xhr = this.createXhrObject();
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState !== 4){
+                return;
+            }
+            if(xhr.status === 200){
+                callback.success(xhr.responseText,xhr.responseXML);
+                this.advanceQueue();
+            }
+            else{
+                callback.failure(xhr.status);
+                setTimeout(function () {
+                    this.request(method,url,callback,postVars);
+                },this.retryDelay*1000);
+            }
+        };
+        if(method !== 'POST')postVars = null;
+        xhr.send(postVars);
     }
 }
+
+QueueHandler.prototype.advanceQueue = function () {
+    if(this.queue.length === 0){
+        this.requestInProgress = false;
+        return;
+    }
+    var req = this.queue.shift();
+    this.request(req.method,req.url,req.callback,req.postVars);
+}
+
+
+//处理器类，应对离线模式
+var OfflineHandler = function () {
+    this.storedRequests = [];
+}
+
+extend(OfflineHandler,SimpleHandler);
+
+OfflineHandler.prototype.request = function (method,url,callback,postVars) {
+    if(XhrManager.isOffline()){
+        this.storedRequests.push({method:method,url:url,callback:callback,postVars:postVars});
+    }
+    else{
+        this.flushRequests();
+        this.superClass.request(method,url,callback,postVars);
+    }
+}
+
+OfflineHandler.prototype.flushRequests = function () {
+    for(var i=0,len=this.storedRequests.length;i<len;i++){
+        var request = this.storedRequests[i];
+        OfflineHandler.superClass.request(request.method,request.url,request.callback,request.postVars);
+    }
+}
+
+
+var XhrManager = {
+
+    //另一个工厂模式，根据实际情况选择创建合适的Xhr对象
+    createXhrObject:function () {
+        var xhr;
+        if(this.isOffline()){
+            xhr = new OfflineHandler();
+        }
+        else if(this.isHighLatency()){
+            xhr = new QueueHandler();
+        }
+        else{
+            xhr = new SimpleHandler();
+        }
+        Interface.ensureImplements(xhr,AjaxHandler);
+        return xhr;
+    },
+    isOffline:function () {
+        //send a request to verify offline or not
+    },
+    isHighLatency:function () {
+        //send some requests to verify whether there is a high latency
+    }
+}
+
+//使用XhrManager
+var handler = XhrManager.createXhrObject();
+callback = {
+    success:function (responseText) {
+        console.log(responseText);
+    },
+    failure:function (responseCode) {
+        console.log(responseCode);
+    }
+}
+handler.request('GET','https://www.baidu.com',callback);
+
